517b9309e82bf99a466f29b240d230f79122a1e4
Rina <huvian11@mail.ru>
2023-03-26 00:59:24 +0300

    Add task10

---
+++ block4task10.py
@@ -0,0 +1,47 @@
+import csv
+import datetime
+import matplotlib.pyplot as plt
+
+
+def parse_time(text):
+    return datetime.datetime.strptime(text, '%Y-%m-%d %H:%M:%S.%f')
+
+
+def load_csv(filename):
+    with open(filename, encoding='utf8') as f:
+        return list(csv.reader(f, delimiter=','))
+
+
+# id, task, variant, group, time
+messages = load_csv('messages.csv')
+
+# id, message_id, time, status
+checks = load_csv('checks.csv')
+
+# task, variant, group, time, status, achievements
+statuses = load_csv('statuses.csv')
+
+achs = {}
+
+for status in statuses:
+    if status[2] not in achs:
+        achs[status[2]] = 0
+    lst = status[5].split(',')
+    if status[2] in achs and lst[0] != '[]' and len(lst) > 1:
+        achs[status[2]] += len(status[5].split(','))
+
+sortedGroupAndCounts = dict(sorted(achs.items(), key=lambda x: x[1]))
+
+groupsLabel = list(sortedGroupAndCounts.keys())
+groupsLabel_ = groupsLabel[-10:]
+groupsLabel_.reverse()
+
+groupsCounts = list(sortedGroupAndCounts.values())
+groupsCounts_ = groupsCounts[-10:]
+groupsCounts_.reverse()
+
+plt.bar(groupsLabel_, groupsCounts_, color='black')
+plt.xticks(rotation=20)
+plt.ylabel('Achievements')
+plt.title('TOP-10')
+plt.show()
