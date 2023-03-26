b2074c7208dd0220e8ad1c3242080ac3d6577ba1
Rina <huvian11@mail.ru>
2023-03-26 15:50:49 +0300

    Edit task2

--- block4task2.py
+++ block4task2.py
@@ -21,13 +21,12 @@ checks = load_csv('checks.csv')
 # task, variant, group, time, status, achievements
 statuses = load_csv('statuses.csv')
 
-param = [parse_time(row[4]) for row in messages] \
-        + [parse_time(row[2]) for row in checks] \
-        + [parse_time(row[3]) for row in statuses]
+param = [parse_time(row[4]) for row in messages]
 
 hours = [val.hour for val in param]
 
-plt.hist(hours,  bins=24, color='black', range=(0, 24))
+plt.hist(hours,  bins=24, color='black', range=(0, 24), rwidth=
+         0.5)
 
 plt.xlabel('Hours')
 plt.ylabel('Activity')
